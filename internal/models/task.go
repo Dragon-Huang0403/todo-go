c06548a5c46dd06c00071fc2de944ce8da439e7f
Dragon Huang <j0918023423@gmail.com>
2024-06-22 10:25:14 +0700

    feat(models): init models

---
+++ internal/models/task.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrConvertFailed = errors.New("failed to convert interface to Task")
+)
+
+type TaskStatus int
+
+const (
+	TaskStatusIncomplete TaskStatus = iota + 1
+	TaskStatusCompleted
+)
+
+type Task struct {
+	ID uuid.UUID `json:"id" validate:"required" format:"uuid"`
+
+	// task name
+	Name string `json:"name" validate:"required" example:"account name"`
+
+	// 0 represents an incomplete task, 1 represents a completed task
+	Status    TaskStatus `json:"status" validate:"required" swaggertype:"integer" example:"0"`
+	CreatedAt time.Time  `json:"created_at" validate:"required" format:"date-time"`
+	UpdatedAt time.Time  `json:"updated_at" validate:"required" format:"date-time"`
+}
+
+func (Task) FromDB(v interface{}) (*Task, error) {
+	task, ok := v.(*Task)
+	if !ok {
+		return nil, ErrConvertFailed
+	}
+	return task, nil
+}
