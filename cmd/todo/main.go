73b87237f3f2a21bef14372736cfd74cf471c6a2
Dragon Huang <j0918023423@gmail.com>
2024-06-22 09:09:03 +0700

    feat(logger): set logger into ctx

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/dragon-huang0403/todo-go/pkg/logger"
 	"github.com/dragon-huang0403/todo-go/pkg/validator"
@@ -17,6 +21,10 @@ func init() {
 }
 
 func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
 	appConfig, err := getAppConfig(configFile)
 	if err != nil {
 		log.Fatalf("failed to get config: %v", err)
@@ -28,10 +36,10 @@ func main() {
 		log.Fatalf("failed to validate config: %v", err)
 	}
 
-	logger, err := logger.New(appConfig.Operation.LogLevel)
+	ctx, err = logger.Init(ctx, appConfig.Operation.LogLevel)
 	if err != nil {
 		log.Fatalf("failed to create logger: %v", err)
 	}
 
-	logger.Info("config", zap.Any("config", appConfig))
+	logger.Info(ctx, "config", zap.Any("config", appConfig))
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,13 +1,16 @@
 package logger
 
 import (
+	"context"
 	"fmt"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func New(_logLevel string) (*zap.Logger, error) {
+type ctxKey struct{}
+
+func Init(ctx context.Context, _logLevel string) (context.Context, error) {
 	logLevel, err := zapcore.ParseLevel(_logLevel)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse log level: %w", err)
@@ -15,5 +18,44 @@ func New(_logLevel string) (*zap.Logger, error) {
 
 	zapConfig := zap.NewProductionConfig()
 	zapConfig.Level = zap.NewAtomicLevelAt(logLevel)
-	return zapConfig.Build()
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build logger: %w", err)
+	}
+
+	ctx = context.WithValue(ctx, ctxKey{}, logger)
+	return ctx, nil
+}
+
+func loggerFromContext(ctx context.Context) *zap.Logger {
+	logger, ok := ctx.Value(ctxKey{}).(*zap.Logger)
+	if !ok {
+		fmt.Println("failed to get logger from context")
+		return zap.NewNop()
+	}
+	return logger
+}
+
+func Debug(ctx context.Context, msg string, fields ...zapcore.Field) {
+	loggerFromContext(ctx).Debug(msg, fields...)
+}
+
+func Info(ctx context.Context, msg string, fields ...zapcore.Field) {
+	loggerFromContext(ctx).Info(msg, fields...)
+}
+
+func Warn(ctx context.Context, msg string, fields ...zapcore.Field) {
+	loggerFromContext(ctx).Warn(msg, fields...)
+}
+
+func Error(ctx context.Context, msg string, fields ...zapcore.Field) {
+	loggerFromContext(ctx).Error(msg, fields...)
+}
+
+func Fatal(ctx context.Context, msg string, fields ...zapcore.Field) {
+	loggerFromContext(ctx).Fatal(msg, fields...)
+}
+
+func Level(ctx context.Context) zapcore.Level {
+	return loggerFromContext(ctx).Level()
 }
